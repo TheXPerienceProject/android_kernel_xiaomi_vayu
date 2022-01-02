5e3b7c1c24406da9444b503c7699fe8f484ad1e8
klozz <klozz@TheXPerienceProject.org>
2022-01-02 07:45:37 -0600

    block: elevator: fix build warning
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- block/elevator.c
+++ block/elevator.c
@@ -236,7 +236,7 @@ int elevator_init(struct request_queue *q, char *name)
 		 */
 
 	        if (q->tag_set && q->tag_set->flags & BLK_MQ_F_NO_SCHED_BY_DEFAULT)
-        		return NULL;
+        		return 0;
 		if (q->mq_ops) {
 			if (q->nr_hw_queues == 1)
 				e = elevator_get(q, "mq-deadline", false);
